83e44044d62bc93e8a07588384a8d85027dc7999
Michael Gilbertson <mggwxyz@gmail.com>
2017-05-18 12:08:24 -0400

    changed app to use h2 in-memory db1

---
+++ src/main/java/todolist/domain/Task.java
@@ -0,0 +1,97 @@
+package todolist.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.*;
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ * A Task.
+ */
+@Entity
+@Table(name = "task")
+@Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
+public class Task implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "name", length = 100, nullable = false)
+    private String name;
+
+    @Size(max = 255)
+    @Column(name = "description", length = 255)
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Task name(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Task description(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Task task = (Task) o;
+        if (task.id == null || id == null) {
+            return false;
+        }
+        return Objects.equals(id, task.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            ", description='" + description + "'" +
+            '}';
+    }
+}
---
+++ src/main/java/todolist/repository/TaskRepository.java
@@ -0,0 +1,15 @@
+package todolist.repository;
+
+import todolist.domain.Task;
+
+import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
+
+/**
+ * Spring Data JPA repository for the Task entity.
+ */
+@SuppressWarnings("unused")
+public interface TaskRepository extends JpaRepository<Task,Long> {
+
+}
